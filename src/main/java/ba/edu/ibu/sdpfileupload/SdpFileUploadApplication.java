891c8af30360f615faaecf02eaf57778b7cb7c49
hamza.bakaran <hamzabakaran@gmail.com>
2024-03-24 10:36:45 +0100

    Initial commit

---
+++ src/main/java/ba/edu/ibu/sdpfileupload/SdpFileUploadApplication.java
@@ -0,0 +1,13 @@
+package ba.edu.ibu.sdpfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdpFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdpFileUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ba/edu/ibu/sdpfileupload/SdpFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.edu.ibu.sdpfileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdpFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
