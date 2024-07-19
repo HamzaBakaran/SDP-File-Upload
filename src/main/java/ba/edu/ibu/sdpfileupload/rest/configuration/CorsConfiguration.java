285e0b0abe6d4c7a4c5a2c9f668c4f4a842b2495
hamza.bakaran <hamzabakaran@gmail.com>
2024-07-19 16:57:16 +0200

    Updated cors configuration

--- src/main/java/ba/edu/ibu/sdpfileupload/rest/configuration/CorsConfiguration.java
+++ src/main/java/ba/edu/ibu/sdpfileupload/rest/configuration/CorsConfiguration.java
@@ -14,6 +14,7 @@ public class CorsConfiguration {
             public void addCorsMappings(CorsRegistry registry) {
                 registry
                         .addMapping("/**")
+                        .allowedOrigins("https://main--sdp-file-upload.netlify.app")
                         .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
